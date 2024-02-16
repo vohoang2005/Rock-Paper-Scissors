46bdbb231a7242ce77b5a83f5330505d200a4268
vohoang2005 <vohoang2005@gmail.com>
2024-02-15 21:51:55 -0800

    Update Image for visualization.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -51,7 +51,10 @@ function playGame(playerMove) {
 
     document.querySelector('.js-result').innerHTML = result;
 
-    document.querySelector('.js-moves').innerHTML = `You ${playerMove} - ${computerMove} Computer`;
+    document.querySelector('.js-moves').innerHTML = `You 
+    <img src="./assets/${playerMove}-emoji.png" class="move-icon">
+    <img src="./assets/${computerMove}-emoji.png" class="move-icon">
+    Computer`;
 }
 
 function updateScoreElement() {
