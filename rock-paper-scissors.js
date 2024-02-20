e25f5902970aa6434e5a84df82eaf666d53cab26
vohoang2005 <vohoang2005@gmail.com>
2024-02-19 19:22:19 -0800

    replace onClick with eventlistener.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -22,6 +22,28 @@ function autoPlay() {
   }
 }
 
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('Rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('Paper');
+});
+
+document.querySelector('.js-scissor-button').addEventListener('click', () => {
+  playGame('Scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('Rock');
+  } else if (event.key === 'p') {
+    playGame('Paper');
+  } else if (event.key === 's') {
+    playGame('Scissors');
+  }
+});
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
